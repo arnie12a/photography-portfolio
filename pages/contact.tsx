ef08e4a88d679bfb39decf1a143b1f2d7709b447
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-01-04 18:33:27 -0600

    added more styling to the contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -32,29 +32,35 @@ export default function Home() {
       <main className="pt-[110px]">
         <div className="flex flex-col items-center h-full">
             <h1 className="text-4xl py-6">Contact Me</h1>
-
-            <div className="mb-4">
-                <label className="block text-sm font-medium mb-2 dark:text-white">Email address</label>
-                <div className="relative">
-                    <input type="text" id="hs-leading-icon" name="hs-leading-icon" className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="you@site.com" />
-                    <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-                    <svg className="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+            <form action="mailto:arnavkarnikphotos@gmail.com" method="post" encType="text/plain">
+                <div className="mb-5">
+                    <label className="block text-sm font-medium mb-2 dark:text-white">Email Address</label>
+                    <div className="relative">
+                        <input type="text" id="hs-leading-icon" name="hs-leading-icon" className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="you@site.com" />
+                        <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+                        <svg className="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="mb-4">
-                <label className="block text-sm font-medium mb-2 dark:text-white">Message</label>
-                <div className="mb-3 pt-0">
-                    <input type="text" placeholder="Message" className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"/>
+                <div className="mb-5">
+                    <label className="block text-sm font-medium mb-2 dark:text-white">Message</label>
+                    <div className="relative">
+                        <textarea placeholder="I want to talk with you about ..." className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" style={{ height: '120px' }}></textarea>
+                        <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+                            <svg className="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+                        </div>
+                    </div>
+                    
                 </div>
-            </div>
+                
+                <div className="mb-6">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Send
+                    </button>
+                </div>
+            </form>
             
-            <div className="mb-6">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                    Send
-                </button>
-            </div>
             
         </div>
     
