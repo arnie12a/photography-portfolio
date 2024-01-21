735036bf1272e0a0e55092c18ffde9d7763918e3
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-01-21 13:19:15 -0600

    new lookout

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -36,15 +36,34 @@ export default function Home() {
               <h2 className="text-2xl font-bold mb-4">Arnav Karnik</h2>
               <p>
                 I am an aspiring photographer. I love exploring new places and capturing its beauty. 
-                My portfolio showcases all my favorite pictures I have taken.
-              </p><br></br>
-              <p>
-                This upcoming month I am traveling to India which I am escatic for. Stay tuned for some 
-                India pictures!
+                My portfolio showcases all my favorite pictures I have taken on all my adventures. 
+                I am looking forward to collaborating with others and seeing where photography takes me!
+              </p><br/>
+              <p className="inline-block relative">
+                This upcoming month I am traveling to &nbsp;
+                <span className="relative inline-block">
+                  <img src="india.png" alt="indian flag" className="inline-block w-8" />
+                  &nbsp;India    
+                </span>
+                &nbsp;
+                and exploring a bunch of places with my parents. 
               </p>
+              <br/><br />
+              <p>Stay tuned...</p>
+
               <br/>
-              <a href="https://www.instagram.com/arnavkarnikphotos/">Instagram</a><br/>
-              <a href="mailto:arnavkarnikphotos@gmail.com">Contact Me</a>
+              <div className="flex justify-center">
+                <a href="https://www.instagram.com/arnavkarnikphotos/" className="py-px pr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="57" height="57" viewBox="0 0 48 48">
+                    <radialGradient id="yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1" cx="19.38" cy="42.035" r="44.899" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fd5"></stop><stop offset=".328" stop-color="#ff543f"></stop><stop offset=".348" stop-color="#fc5245"></stop><stop offset=".504" stop-color="#e64771"></stop><stop offset=".643" stop-color="#d53e91"></stop><stop offset=".761" stop-color="#cc39a4"></stop><stop offset=".841" stop-color="#c837ab"></stop></radialGradient><path fill="url(#yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1)" d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"></path><radialGradient id="yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2" cx="11.786" cy="5.54" r="29.813" gradientTransform="matrix(1 0 0 .6663 0 1.849)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#4168c9"></stop><stop offset=".999" stop-color="#4168c9" stop-opacity="0"></stop></radialGradient><path fill="url(#yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2)" d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"></path><path fill="#fff" d="M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z"></path><circle cx="31.5" cy="16.5" r="1.5" fill="#fff"></circle><path fill="#fff" d="M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z"></path>
+                  </svg>
+                </a>
+                <br/>
+                <a href="mailto:arnavkarnikphotos@gmail.com" className="pt-2 pl-2">
+                  <img src="email.svg" alt="email svg" className="w-11"/>
+                </a>
+              </div>
+              
             </div>
           </div>
 
