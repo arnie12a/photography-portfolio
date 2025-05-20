33c7850cd31363ca947f9ceaf394e1e2aaa20f9e
Arnav Karnik <arnavkarnik12@Arnavs-MacBook-Pro.local>
2025-05-19 21:29:05 -0500

    added stuff to about me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -421,10 +421,8 @@ export default function TempIndex() {
                     <div className="md:w-1/2 md:pl-8">
                     <h2 className="text-2xl font-bold mb-4">My name is Arnav Karnik</h2>
                     <p>
-                        
-                        I am an aspiring travel photographer. I love exploring new places and capturing their beauty. 
-                        My portfolio showcases my favorite landscape, portrait, and structure photos from these unqiue locations. 
-                        I hope to improve my technique and my storytelling as I bring you along on my future adventures!
+                    I'm a photographer who loves to capture meaningful moments. My passion lies in photographing the beauty of nature during my many adventures. 
+                    I look forward to discovering new places and continuing to tell stories through the lens.
                     </p><br/>
 
                     <br/>
